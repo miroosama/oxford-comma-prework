264c337801e88b21b50934cd49c03d9f065f5c01
osama miro <o.miro650@gmail.com>
2017-12-18 21:11:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
 array.join
 elsif array.length == 2
   array.join(" and ")
-elsif 
-  array[0...-1].join(', ') + " and " + array[-1]
+elsif
+  array[0..-2].join(', ') + " and " + array[-1]
 end
 end
